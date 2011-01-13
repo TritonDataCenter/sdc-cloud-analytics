eb60bfecd506f9ae4294f285f6006a34ee6b002d
Dave Pacheco <dap@joyent.com>
2011-01-13 00:19:18 +0000

    INTRO-106 update CA demo to use new proxy

--- demo/basicvis/cademo.js
+++ demo/basicvis/cademo.js
@@ -1,5 +1,19 @@
 /*
  * demo.js: static-file node HTTP server for demos
+ *
+ * Usage: node cademo.js [[-P] server]
+ *
+ *    Sets up a web server on port 23183 for the CA demo.
+ *
+ *    If no server is specified, the demo uses a configuration service and
+ *    aggregator on the same host as the demo.
+ *
+ *    If a server is specified with -P, the demo uses a configuration service
+ *    and aggregator service on the hostname specified by "server".
+ *
+ *    If a server is specified but -P is not specified, the demo uses a
+ *    proxy on "server" port 80 that is assumed to automatically vector to the
+ *    appropriate service/port.
  */
 
 var mod_http = require('http');
@@ -11,12 +25,33 @@ var dd_index = 'graph.htm';
 var dd_cwd = __dirname;
 var dd_port = 23183;
 var dd_vars = [];
+var dd_useproxy = false;
+var dd_server;
 
-if (process.argv.length > 2) {
-	var server = process.argv[2];
+var opt_P;
 
-	console.log('Cloud analytics server set to ' + server);
-	dd_vars.push('gServer = "' + server + '"');
+var ii;
+
+for (ii = 2; ii < process.argv.length; ii++) {
+	if (process.argv[ii] == '-P') {
+		opt_P = true;
+		continue;
+	}
+
+	dd_server = process.argv[ii];
+}
+
+if (dd_server) {
+	console.log('Cloud analytics server set to ' + dd_server);
+	dd_vars.push('gServer = "' + dd_server + '"');
+	if (!opt_P)
+		dd_useproxy = true;
+}
+
+if (dd_useproxy) {
+	console.log('Using proxy server on port 80.');
+	dd_vars.push('gPortConfigsvc = 80');
+	dd_vars.push('gPortAggsvc = 80');
 }
 
 mod_http.createServer(function (req, res) {

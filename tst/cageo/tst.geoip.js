9136d7942d6ef5c704654d669dcf2541983c16be
Dave Pacheco <dap@joyent.com>
2011-03-11 22:18:12 +0000

    INTRO-183 tst.geoip.js should always run

--- tst/cageo/tst.geoip.js
+++ tst/cageo/tst.geoip.js
@@ -2,14 +2,4 @@ var mod_cageoip = require('../../lib/ca/ca-geo');
 var sys = require('sys');
 var assert = require('assert');
 
-/*
- * If we don't have GEOIP_DATABASE in the enviornment, we will explicitly
- * skip this test.  Set this to be a GeoLite.dat file to get this to
- * operate properly.
- */
-if (!process.env['GEOIP_DATABASE']) {
-	sys.puts('GEOIP_DATABASE is unset; skipping');
-	process.exit(0);
-}
-
 sys.puts(sys.inspect(mod_cageoip.caGeoIP('138.16.60.2')));

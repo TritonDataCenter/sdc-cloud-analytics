8ef791f89da1aab9e815b64ec204c9299e90f492
Robert Mustacchi <rm@joyent.com>
2011-03-17 08:12:12 +0000

    INTRO-315 revamp disk I/O metric INTRO-338 align catest number output

--- cmd/cainst/modules/dtrace/io-ops.js
+++ cmd/cainst/modules/dtrace/io-ops.js
@@ -9,64 +9,68 @@ var desc = {
     label: 'operations',
     type: 'ops',
     fields: {
-	hostname: { label: 'hostname', type: mod_ca.ca_type_string },
-	zonename: { label: 'zone name', type: mod_ca.ca_type_string },
-	optype: { label: 'type', type: mod_ca.ca_type_string },
-	execname: { label: 'application name',
-	    type: mod_ca.ca_type_string },
-	latency: { label: 'latency', type: mod_ca.ca_type_latency },
-	pid: { label: 'process identifier', type: mod_ca.ca_type_string }
+	hostname: {
+	    label: 'hostname',
+	    type: mod_ca.ca_type_string
+	},
+	optype: {
+	    label: 'type',
+	    type: mod_ca.ca_type_string
+	},
+	latency: {
+	    label: 'latency',
+	    type: mod_ca.ca_type_latency
+	},
+	size: {
+	    label: 'size',
+	    type: mod_ca.ca_type_number
+	},
+	name: {
+	    label: 'device name',
+	    type: mod_ca.ca_type_string
+	},
+	offset: {
+	    label: 'block offset',
+	    type: mod_ca.ca_type_number
+	}
     },
     metad: {
 	probedesc: [ {
 	    probes: [ 'io:::start' ],
 	    gather: {
-		zonename: {
-		    gather: 'zonename',
-		    store: 'global[arg0]'
-		}, execname: {
-		    gather: 'execname',
-		    store: 'global[arg0]'
-		}, latency: {
+		latency: {
 		    gather: 'timestamp',
-		    store: 'global[arg0]'
-		}, pid: {
-		    gather: 'pid',
 		    store: 'global[arg0]'
 		}
 	    }
 	}, {
 	    probes: [ 'io:::done' ],
 	    aggregate: {
-		zonename: 'count()',
 		optype: 'count()',
 		hostname: 'count()',
-		execname: 'count()',
 		latency: 'llquantize($0, 10, 3, 11, 100)',
-		pid: 'count()',
+		size: 'llquantize($0, 10, 3, 11, 100)',
+		name: 'count()',
+		offset: 'llquantize($0, 10, 0, 11, 100)',
 		default: 'count()'
 	    },
 	    transforms: {
-		zonename: '$0[arg0]',
 		optype: '(args[0]->b_flags & B_READ ? "read" : "write")',
 		hostname: '"' + mod_ca.caSysinfo().ca_hostname + '"',
-		execname: '$0[arg0]',
 		latency: 'timestamp - $0[arg0]',
-		pid: 'lltostr($0[arg0])'
+		name: 'args[1]->dev_statname',
+		size: 'args[0]->b_bcount',
+		offset: 'args[0]->b_blkno'
 	    },
 	    verify: {
-		zonename: '$0[arg0]',
-		execname: '$0[arg0]',
-		latency: '$0[arg0]',
-		pid: '$0[arg0]'
-	    }
+		latency: '$0[arg0]'
+	    },
+	    predicate: 'args[1]->dev_name == "sd" || ' +
+		'args[1]->dev_name == "cmdk"'
 	}, {
 	    probes: [ 'io:::done' ],
 	    clean: {
-		execname: '$0[arg0]',
-		zonename: '$0[arg0]',
-		latency: '$0[arg0]',
-		pid: '$0[arg0]'
+		latency: '$0[arg0]'
 	    }
 	} ]
     }

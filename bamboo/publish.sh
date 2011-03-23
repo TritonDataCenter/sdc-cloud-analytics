427584ca8eab0ff106ebdd83ca985620b77b4a66
Orlando Vazquez <ovazquez@gmail.com>
2011-03-23 10:37:50 -0700

    AGENT-130: Remove space character from filename

--- bamboo/publish.sh
+++ bamboo/publish.sh
@@ -3,7 +3,7 @@
 if [[ `hostname` = 'bh1-autobuild' ]]; then
   pfexec mkdir -p $PUBLISH_LOCATION
   pfexec cp build/pkg/cabase.tar.gz    "$PUBLISH_LOCATION/$CABASE_PKG"
-  pfexec cp build/pkg/cainstsvc.tar.gz "$PUBLISH_LOCATION/$CAINSTSVC_PKG "
+  pfexec cp build/pkg/cainstsvc.tar.gz "$PUBLISH_LOCATION/$CAINSTSVC_PKG"
 else
   echo "Not publishing because not on bh1-autobuild"
 fi

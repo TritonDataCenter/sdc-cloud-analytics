bb64d67895d73b2fc1734bc9451f1e521aad4b07
Jim Pick <jim@joyent.com>
2011-04-04 21:58:28 -0700

    INTRO-372 Modify upload script to work on a different server.
    Moving the CA build off of bh1-autobuild onto bh1-smartosbuild (bldzone2).

--- bamboo/publish.sh
+++ bamboo/publish.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [[ `hostname` = 'bh1-autobuild' ]]; then
-  pfexec mkdir -p $PUBLISH_LOCATION
-  pfexec cp build/pkg/cabase.tar.gz    "$PUBLISH_LOCATION/$CABASE_PKG"
-  pfexec cp build/pkg/cainstsvc.tar.gz "$PUBLISH_LOCATION/$CAINSTSVC_PKG"
+if [[ `hostname` = 'bldzone2.joyent.us' ]]; then
+  ssh bamboo@10.2.0.190 mkdir -p $PUBLISH_LOCATION
+  scp build/pkg/cabase.tar.gz    "bamboo@10.2.0.190:$PUBLISH_LOCATION/$CABASE_PKG"
+  scp build/pkg/cainstsvc.tar.gz "bamboo@10.2.0.190:$PUBLISH_LOCATION/$CAINSTSVC_PKG"
 else
-  echo "Not publishing because not on bh1-autobuild"
+  echo "Not publishing because not on bldzone2.joyent.us (bh1-smartosbuild)"
 fi

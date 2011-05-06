cc314387864357604f72e04cdcba2f1b9e1f3a4e
Dave Pacheco <dap@joyent.com>
2011-05-06 15:20:00 -0700

    fix test again

--- tst/library/tst.error_system.js
+++ tst/library/tst.error_system.js
@@ -6,6 +6,7 @@ var mod_assert = require('assert');
 var mod_fs = require('fs');
 var mod_ca = require('../../lib/ca/ca-common.js');
 
+var re = new RegExp(process.env['SRC'], 'g');
 var exn;
 
 console.log('TEST: EEXIST error');
@@ -19,7 +20,7 @@ mod_assert.ok(exn);
 mod_assert.ok(exn instanceof caSystemError);
 mod_assert.equal(exn.code(), ECA_NOENT);
 mod_assert.equal(exn.syscode(), 'ENOENT');
-console.log(caSprintf('%r', exn));
+console.log(caSprintf('%r', exn).replace(re, '...'));
 
 console.log('TEST: ENOENT error');
 try {
@@ -32,4 +33,4 @@ mod_assert.ok(exn);
 mod_assert.ok(exn instanceof caSystemError);
 mod_assert.equal(exn.code(), ECA_EXISTS);
 mod_assert.equal(exn.syscode(), 'EEXIST');
-console.log(caSprintf('%r', exn));
+console.log(caSprintf('%r', exn).replace(re, '...'));

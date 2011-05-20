dbd83197f6dfa009ea7d5cd072dd9cce7e28a4ab
Orlando Vazquez <ovazquez@gmail.com>
2011-05-20 06:57:25 -0700

    INTRO-423: Remove build machine check from publish script

--- bamboo/publish.sh
+++ bamboo/publish.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-if [[ `hostname` = 'bldzone2.joyent.us' ]]; then
-  ssh bamboo@10.2.0.190 mkdir -p $PUBLISH_LOCATION
-  scp build/pkg/cabase.tar.gz    "bamboo@10.2.0.190:$PUBLISH_LOCATION/$CABASE_PKG"
-  scp build/pkg/cainstsvc.tar.gz "bamboo@10.2.0.190:$PUBLISH_LOCATION/$CAINSTSVC_PKG"
-else
-  echo "Not publishing because not on bldzone2.joyent.us (bh1-smartosbuild)"
-fi
+ssh bamboo@10.2.0.190 mkdir -p $PUBLISH_LOCATION
+scp build/pkg/cabase.tar.gz    "bamboo@10.2.0.190:$PUBLISH_LOCATION/$CABASE_PKG"
+scp build/pkg/cainstsvc.tar.gz "bamboo@10.2.0.190:$PUBLISH_LOCATION/$CAINSTSVC_PKG"

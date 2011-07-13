f437e3b90d93c5d6b694001f574a519eed70ec56
Robert Mustacchi <rm@joyent.com>
2011-07-13 00:46:23 +0000

    INTRO-479 Creating AND Predicate with empty predicate blows up

---
+++ tst/pred/tst.print.js
@@ -0,0 +1,40 @@
+var mod_capred = require('../../lib/ca/ca-pred');
+
+var test_cases = [
+	{},
+	{
+		eq: [ "execname", "mplayer" ]
+	},
+	{
+		ne: [ "execname", "vlc" ]
+	},
+	{
+		lt: [ "latency", 42 ]
+	},
+	{
+		gt: [ "latency", 23 ]
+	},
+	{
+		le: [ "latency", 42 ]
+	},
+	{
+		ge: [ "latency", 23 ]
+	},
+	{
+		and: [ {}, { eq: [ "latency", 23 ] } ]
+	},
+	{
+		or: [ { eq: [ "latency", 23 ] }, {} ]
+	}
+];
+
+function main()
+{
+	var ii;
+
+	for (ii = 0; ii < test_cases.length; ii++) {
+		console.log(mod_capred.caPredPrint(test_cases[ii]));
+	}
+}
+
+main();

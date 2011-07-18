6cf38981709e58967944c463ef04b074dfff9fe2
John Sonnenschein <johns@joyent.com>
2011-07-18 12:39:15 -0700

    [AGENT-236] amon, CA and smartlogin should leave their artifacts in a configurable location

--- bamboo/build.sh
+++ bamboo/build.sh
@@ -6,6 +6,7 @@ set -o xtrace
 DIRNAME=$(cd `dirname $0`; pwd)
 git submodule update --init
 gmake pkg
+gmake release
 
 NAME=cloud_analytics
 BRANCH=$(git symbolic-ref HEAD | cut -d'/' -f3)
@@ -14,6 +15,19 @@ BUILDSTAMP=`TZ=UTC date "+%Y%m%dT%H%M%SZ"`; export BUILDSTAMP
 PKG_SUFFIX=${BRANCH}-${BUILDSTAMP}-${DESCRIBE}.tgz
 CABASE_PKG=cabase-${PKG_SUFFIX}
 CAINSTSVC_PKG=cainstsvc-${PKG_SUFFIX}
-PUBLISH_LOCATION=/rpool/data/coal/live_147/agents/${NAME}/${BRANCH}/
+CA_PKG=ca-pkg-${BRANCH}-${DESCRIBE}*.tar.bz2 
 
+if [[ $( echo $BRANCH | grep release) && -z $PUBLISH_LOCATION ]]; then
+    releasedate=$(echo $BRANCH | cut -d '-' -f2)
+    RELEASEDIR=${releasedate:0:4}-${releasedate:4:2}-${releasedate:6:2}
+    ASSETS_LOCATION=/rpool/data/coal/releases/${RELEASEDIR}/deps/
+    PUBLISH_LOCATION=${ASSETS_LOCATION}/${NAME}/${BRANCH}/
+fi
+
+if [[ -z $PUBLISH_LOCATION ]]; then
+  PUBLISH_LOCATION=/rpool/data/coal/live_147/agents/${NAME}/${BRANCH}/
+fi
+if [[ -z $ASSETS_LOCATION ]]; then
+  ASSETS_LOCATION=/rpool/data/coal/live_147/assets
+fi
 source $DIRNAME/publish.sh
--- bamboo/publish.sh
+++ bamboo/publish.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-ssh bamboo@10.2.0.190 mkdir -p $PUBLISH_LOCATION
-scp build/pkg/cabase.tar.gz    "bamboo@10.2.0.190:$PUBLISH_LOCATION/$CABASE_PKG"
-scp build/pkg/cainstsvc.tar.gz "bamboo@10.2.0.190:$PUBLISH_LOCATION/$CAINSTSVC_PKG"
+if [[ `hostname` == 'bldzone2.joyent.us' ]] ; then 
+    ssh bamboo@10.2.0.190 mkdir -p $PUBLISH_LOCATION
+    ssh bamboo@10.2.0.190 mkdir -p $ASSETS_LOCATION
+    scp build/pkg/cabase.tar.gz    "bamboo@10.2.0.190:$PUBLISH_LOCATION/$CABASE_PKG"
+    scp build/pkg/cainstsvc.tar.gz "bamboo@10.2.0.190:$PUBLISH_LOCATION/$CAINSTSVC_PKG"
+    scp build/dist/$CA_PKG "bamboo@10.2.0.190:$ASSETS_LOCATION/$CA_PKG"
+else
+    pfexec mkdir -p $PUBLISH_LOCATION
+    pfexec mkdir -p $ASSETS_LOCATION
+    pfexec cp build/pkg/cabase.tar.gz    $PUBLISH_LOCATION/$CABASE_PKG
+    pfexec cp build/pkg/cainstsvc.tar.gz $PUBLISH_LOCATION/$CAINSTSVC_PKG
+    pfexec cp build/dist/$CA_PKG $ASSETS_LOCATION/$CA_PKG
+fi

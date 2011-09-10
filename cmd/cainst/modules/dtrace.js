e9994e0b3bfc6e6bfed64e967acb0c9d9374c14c
Dave Pacheco <dap@joyent.com>
2011-09-09 17:27:14 -0700

    INTRO-537 Create a TCP accepts metric to use in the portal dashboard.

--- cmd/cainst/modules/dtrace.js
+++ cmd/cainst/modules/dtrace.js
@@ -23,7 +23,8 @@ var insd_metrics = [ 'syscall-ops', 'node-gc_ops', 'node-httpd_ops',
     'cpu-thread_executions', 'fs-logical_ops', 'mysql-query_ops',
     'mysql-connect_ops', 'mysql-filesort_ops', 'mysql-command_ops',
     'mysql-statement_ops', 'cpu-thread_samples', 'fs-rw_ops',
-    'vm-thread_samples', 'vm-irqs', 'vm-physio_ops', 'vm-physio_bytes' ];
+    'vm-thread_samples', 'vm-irqs', 'vm-physio_ops', 'vm-physio_bytes',
+    'tcp-accepts', 'tcp-connects' ];
 
 if (process.env['DTRACE_LIBPATH']) {
 	insd_dt_libpath = process.env['DTRACE_LIBPATH'].split(':');
---
+++ cmd/cainst/modules/dtrace/tcp-accepts.js
@@ -0,0 +1,92 @@
+/*
+ * DTrace metric for TCP accepts, using the syscall provider.
+ */
+var mod_ca = require('../../../../lib/ca/ca-common');
+
+var desc = {
+    module: 'tcp',
+    stat: 'accepts',
+    fields: [ 'hostname', 'zonename', 'pid', 'execname', 'psargs', 'ppid',
+	'pexecname', 'ppsargs', 'rport', 'raddr' ],
+    fields_internal: [ 'sockaddr' ],
+    metad: {
+	locals: [
+	    { addrtype: 'uint16_t' }
+	],
+	probedesc: [
+	    {
+		probes: [ 'syscall::accept:entry' ],
+		alwaysgather: {
+			sockaddr: {
+				gather: 'arg1',
+				store: 'thread'
+			}
+		}
+	    },
+	    {
+		probes: [ 'syscall::accept:return' ],
+		local: [ {
+		    addrtype: '*(uint16_t *)copyin(self->sockaddr0, \n' +
+			'sizeof (uint16_t))'
+		} ],
+		predicate: 'arg1 >= 0 && (this->addrtype == AF_INET || ' +
+		    'this->addrtype == AF_INET6)',
+		aggregate: {
+			default: 'count()',
+			hostname: 'count()',
+			zonename: 'count()',
+			pid: 'count()',
+			execname: 'count()',
+			psargs: 'count()',
+			ppid: 'count()',
+			pexecname: 'count()',
+			ppsargs: 'count()',
+			rport: 'count()',
+			raddr: 'count()'
+		},
+		transforms: {
+			hostname:
+			    '"' + mod_ca.caSysinfo().ca_hostname + '"',
+			zonename: 'zonename',
+			pid: 'lltostr(pid)',
+			execname: 'execname',
+			psargs: 'curpsinfo->pr_psargs',
+			ppid: 'lltostr(ppid)',
+			pexecname: 'curthread->t_procp->p_parent->' +
+			    'p_user.u_comm',
+			ppsargs:
+			    'curthread->t_procp->p_parent->p_user.u_psargs',
+			rport: [
+			    'ntohs((this->addrtype == AF_INET) ? ',
+			    '((struct sockaddr_in *)copyin(self->sockaddr0, ',
+			    '    sizeof (struct sockaddr_in *)))->sin_port : ',
+			    '((struct sockaddr_in6 *)copyin(self->sockaddr0, ',
+			    '    sizeof (struct sockaddr_in6 *)))->sin6_port)'
+			].join('\n'),
+			raddr: [
+			    '(this->addrtype == AF_INET) ? ',
+			    'inet_ntoa((ipaddr_t *)',
+			    '    &(((struct sockaddr_in *)copyin(',
+			    '        self->sockaddr0, ',
+			    '    sizeof (struct sockaddr_in *)))->sin_addr)) :',
+			    ' inet_ntoa6((in6_addr_t *)',
+			    '    &(((struct sockaddr_in6 *)copyin(',
+			    'self->sockaddr0, ',
+			    '    sizeof (struct sockaddr_in6 *)))->sin6_addr))'
+			].join('\n')
+		},
+		verify: {
+			sockaddr: '$0'
+		}
+	    },
+	    {
+		probes: [ 'syscall::accept:return' ],
+		clean: {
+			sockaddr: '$0'
+		}
+	    }
+	]
+    }
+};
+
+exports.cadMetricDesc = desc;

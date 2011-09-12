88a32beeb121a7835df11d7ee7abfb77199d8af3
John Sonnenschein <johns@joyent.com>
2011-09-12 15:14:05 -0700

    [RELENG-194] ca's bamboo publish should be uploading to <url>/assets/ instead of <url>

--- bamboo/build.sh
+++ bamboo/build.sh
@@ -20,7 +20,7 @@ CA_PKG=ca-pkg-${BRANCH}-${DESCRIBE}*.tar.bz2
 if [[ $( echo $BRANCH | grep release) && -z $PUBLISH_LOCATION ]]; then
     releasedate=$(echo $BRANCH | cut -d '-' -f2)
     RELEASEDIR=${releasedate:0:4}-${releasedate:4:2}-${releasedate:6:2}
-    ASSETS_LOCATION=/rpool/data/coal/releases/${RELEASEDIR}/deps/
+    ASSETS_LOCATION=/rpool/data/coal/releases/${RELEASEDIR}/deps/assets
     PUBLISH_LOCATION=${ASSETS_LOCATION}/agents/${NAME}/${BRANCH}/
 fi
 

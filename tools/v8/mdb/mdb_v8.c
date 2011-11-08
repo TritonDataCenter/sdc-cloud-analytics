b5b056e04e14f088ce39b529a99905dccca138f3
Dave Pacheco <dap@joyent.com>
2011-11-08 14:13:14 -0800

    INTRO-582 v8 dtrace helper should handle more cases

--- tools/v8/mdb/mdb_v8.c
+++ tools/v8/mdb/mdb_v8.c
@@ -1,4 +1,28 @@
 /*
+ * CDDL HEADER START
+ *
+ * The contents of this file are subject to the terms of the
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
+ *
+ * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
+ * or http://www.opensolaris.org/os/licensing.
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL HEADER in each
+ * file and include the License file at usr/src/OPENSOLARIS.LICENSE.
+ * If applicable, add the following below this CDDL HEADER, with the
+ * fields enclosed by brackets "[]" replaced with your own identifying
+ * information: Portions Copyright [yyyy] [name of copyright owner]
+ *
+ * CDDL HEADER END
+ */
+/*
+ * Copyright (c) 2011, Joyent, Inc. All rights reserved.
+ */
+
+/*
  * mdb(1M) module for debugging the V8 JavaScript engine.  This implementation
  * makes heavy use of metadata defined in the V8 binary for inspecting in-memory
  * structures.
@@ -1403,11 +1427,7 @@ static const mdb_walker_t v8_mdb_walkers[] = {
 	{ NULL }
 };
 
-static mdb_modinfo_t v8_mdb = {
-	.mi_dvers = MDB_API_VERSION,
-	.mi_dcmds = v8_mdb_dcmds,
-	.mi_walkers = v8_mdb_walkers
-};
+static mdb_modinfo_t v8_mdb = { MDB_API_VERSION, v8_mdb_dcmds, v8_mdb_walkers };
 
 const mdb_modinfo_t *
 _mdb_init(void)
